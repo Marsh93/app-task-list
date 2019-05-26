6bf05835516d6b59970039aebcfab936dc2bf6b5
OmarZavala <alexoamr1993@gmail.com>
2019-05-26 03:17:47 -0500

    Se agrego funcionalidad de editar y eliminar tarea

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ new Vue({
     data: {
         lists: [],
         newKeep: '',
+        editar: {
+            item: '',
+            index: '',
+        }
     }, 
     methods: {
+        // Método que agrega una nueva tarea
         addKeep: function () {
             this.lists.push(
                 { keep: this.newKeep, completed:false }
             );
             this.newKeep = '';
         },
+        // Método que cambia el estado de una tarea al hacer click
         changeState: function (index) {
             this.lists[index].completed = !this.lists[index].completed;
+        },
+        // Método para editar tarea
+        activeEditKeep: function (index) {
+            this.editar.item = this.lists[index].keep;
+            this.editar.index = index;
+        },
+        editKeep: function () {
+            this.lists[this.editar.index].keep = this.editar.item;
+        },
+        // Método para eliminar la tarea
+        deleteKeep: function (index) {
+            this.lists.splice(index, 1);
         }
     },
 });
